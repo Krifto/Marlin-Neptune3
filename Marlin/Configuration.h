3d4d017275dafb7a53c19fa9cf76881218091da9
Krifto <6061618+Krifto@users.noreply.github.com>
2022-10-18 12:19:17 +0200

    chore: translated chinese comments

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -939,7 +939,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 200, 200, 6, 60 }    //1------最高速度
+#define DEFAULT_MAX_FEEDRATE          { 200, 200, 6, 60 }    //1------最高速度="Maximum speed"
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -952,7 +952,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1000, 800, 100, 1000 }     //1------最高加速度
+#define DEFAULT_MAX_ACCELERATION      { 1000, 800, 100, 1000 }     //1------最高加速度="Highest acceleration"
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
